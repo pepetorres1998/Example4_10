f2922de97482cd94afc8e90528cfe34611e73841
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2018-05-06 23:00:33 -0500

    Seleccion-completada - falta encontrar mayor peso

--- RandomBinario.py
+++ RandomBinario.py
@@ -32,6 +32,7 @@ Terremoto = h.Terremoto()
 poder_list = [GolpeFuego, BolaFuego, Llamarada, Explosion, GolpeRayo, BolaRayo, Relampago, Tormenta,
             GolpeAgua, BolaAgua, Ciclon, Tsunami, GolpeAire, BolaAire, Ventisca, Tornado,
             GolpeRoca, BolaTierra, Derrumbe, Terremoto]
+iter = int(sys.argv[2])
 a = sys.argv[1]
 
 def generate_binary(a):
@@ -72,58 +73,86 @@ def poderes_main(poder, pos, h1, h2):
             if(j in pos[i]):
                 if(c.Peso(h1, h2, poder[j]) == 0):
                     peso_cromosomas.append(0)
-                    print('if')
-                    print(peso_cromosomas)
+                    #print('if')
+                    #print(peso_cromosomas)
                     break
                 else:
                     peso_total = peso_total+c.Peso(h1, h2, poder[j])
-                    print('else')
-                    print(peso_total)
+                    #print('else')
+                    #print(peso_total)
             if(j in pos[i] and c.Peso(h1, h2, poder[j]) == 0):#Rompe el for del cromosoma si es igual a 0
                 break
         if(j in pos[i] and c.Peso(h1, h2, poder[j]) == 0): #Continua con la sig iteracion de la lista si es igual a 0
             continue
-        print(peso_total)
+        #print(peso_total)
         peso_cromosomas.append(peso_total)
-        print(peso_cromosomas)
+        #print(peso_cromosomas)
     return peso_cromosomas
 
 def seleccion_cromosomas(peso_cromo, peso_gen):
-    nueva_generacion = []
     generacion_temporal = []
     cromo_prob = []
     for i in peso_cromo:
         cromo_prob.append(i/peso_gen)
-    print(cromo_prob)
-    print(sum(cromo_prob))
+    #print(cromo_prob)
+    #print(sum(cromo_prob))
     while(len(generacion_temporal) < 20):
         ruleta = random.random()
-        print(ruleta) #sumar valores de cromo_prob y si es mayor que ruleta, es el cromosoma elegido
+        #print(ruleta) #sumar valores de cromo_prob y si es mayor que ruleta, es el cromosoma elegido
         ruleta_sum = 0
         for i in range(len(cromo_prob)):
             ruleta_sum = ruleta_sum+cromo_prob[i]
-            print(ruleta_sum)
+            #print(ruleta_sum)
             if(ruleta_sum > ruleta):
                 generacion_temporal.append(blist[i])
-                print(generacion_temporal)
+                #print(generacion_temporal)
                 break
             else:
-                print('still minor')
+                #print('still minor')
                 continue
-    return(generacion_temporal)
-
+    #print(generacion_temporal)
+    for i in range(0, len(generacion_temporal), 2):
+        r_temp = random.randint(1, 20)
+        t1_inicio = generacion_temporal[i][:r_temp]
+        t1_final = generacion_temporal[i][r_temp:]
+        t2_inicio = generacion_temporal[i+1][:r_temp]
+        t2_final = generacion_temporal[i+1][r_temp:]
+        generacion_temporal[i] = t1_inicio+t2_final
+        generacion_temporal[i+1] = t2_inicio+t1_final
+    #print()
+    #print(generacion_temporal)
+    for i in range(len(generacion_temporal)):
+        tlist = list(generacion_temporal[i])
+        for j in range(len(tlist)):
+            r_temp = random.randint(1, 11)
+            #print(r_temp)
+            if(r_temp == 1):
+                #print('changed ', j)
+                #print(tlist)
+                #print(tlist[j])
+                if(tlist[j] == '0'):
+                    tlist[j] = '1'
+                else:
+                    tlist[j] = '0'
+                #print(tlist[j])
+                #print(tlist)
+        generacion_temporal[i] = "".join(tlist)
+    #print()
+    return generacion_temporal
 #Esto es un comentario para probar si el commit se aplica y se diferencian las branches
 #GRACIAS :* SI FUNCIONAAAA!!!!
 
-blist = generate_binary(a)
-pos = posicion_true(blist)
-
-print(blist)
-print(pos)
-print()
-print()
-peso_cromosomas = poderes_main(poder_list, pos, c.k1, c.s2)
-print(peso_cromosomas)
-peso_generacion = sum(peso_cromosomas)
-print(peso_generacion)
-print(sorted(seleccion_cromosomas(peso_cromosomas, peso_generacion)))
+for i in range(iter):
+    blist = generate_binary(a)
+    pos = posicion_true(blist)
+    print(blist)
+    #print(pos)
+    print()
+    #print()
+    peso_cromosomas = poderes_main(poder_list, pos, c.k1, c.s2)
+    #print(peso_cromosomas)
+    peso_generacion = sum(peso_cromosomas)
+    #print(peso_generacion)
+    nueva_generacion = seleccion_cromosomas(peso_cromosomas, peso_generacion)
+    print(nueva_generacion)
+    print()
